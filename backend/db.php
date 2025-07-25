d3b724cae0305bb7c42cb15bf351d86b4ffbe8a5
Yousuf7900 <yousufislam7900@gmail.com>
2025-07-26 03:27:53 +0600

    database connection

--- backend/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $connection = new mysqli("localhost", "root", "", "campusconnect");
-    if ($connection -> connect_error){
-        die ("Connection failed: " . $connection-> connect_error);
-    }
-?>
