9f72dedb323fcb12d4b9c6c884bd1032d1335b7c
Yousuf7900 <yousufislam7900@gmail.com>
2025-07-26 21:21:06 +0600

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.php", "./includes/**/*.php"], // tell Tailwind where to look for class names
+  content: ["./*.php", "./includes/**/*.php"],
   theme: {
     extend: {},
   },
