347d0aae58e6762da08996972f05418a7794fa7e
Ishtiak Ahmed <ishtiak15-4719@diu.edu.bd>
2025-08-01 23:11:40 +0600

    log in functtionality added, as uer can log in with their credentials with they register.

--- includes/functions.php
+++ includes/functions.php
@@ -13,4 +13,17 @@
 
         return $conn->query("INSERT INTO users (first_name, last_name, email, department, password) VALUES ('$fname', '$lname', '$email', '$dept', '$pass')");
 }
+
+    function login($email, $password) {
+        global $conn;
+        $res = $conn->query("SELECT * FROM users WHERE email='$email'");
+        if ($res->num_rows > 0) {
+            $row = $res->fetch_assoc();
+            if (password_verify($password, $row['password'])) {
+                $_SESSION['user'] = $row;
+                return true;
+            }
+        }
+        return false;
+    }
 ?>
